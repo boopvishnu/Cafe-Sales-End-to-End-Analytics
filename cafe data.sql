7829dad1563c4d02ddf2343f631e5d28ebbf5254
Vishnu Sharma <eichilenovo@gmail.com>
2026-07-18 17:38:10 +0530

    Update item names and locations in sales_data
    Added SQL queries to update item names and locations based on price and null values.

--- cafe data.sql
+++ cafe data.sql
@@ -27,5 +27,41 @@ select count(*) from sales_data where Payment_Method = "Digital Wallet"; /* 2291
 select distinct Location from sales_data;
 select * from sales_data;
 
+select distinct Item,Price_Per_Unit from sales_data;
+
+select * from sales_data where Item="na";
 
 
+set SQL_SAFE_UPDATES=0;
+
+select item from sales_data where Price_Per_Unit = 1;
+update sales_data set Item = "Cookie" where Price_Per_Unit =1;
+
+select item from sales_data where Price_Per_Unit = 1.5;
+update sales_data set Item = "Tea" where Price_Per_Unit =1.5;
+
+select item from sales_data where Price_Per_Unit = 2;
+update sales_data set Item = "Coffee" where Price_Per_Unit =2;
+
+select item from sales_data where Price_Per_Unit = 3;
+update sales_data set Item = "Cake" where Price_Per_Unit =3;
+
+select item from sales_data where Price_Per_Unit = 5;
+update sales_data set Item = "Salad" where Price_Per_Unit =5;
+
+select item from sales_data where Price_Per_Unit = 4;
+update sales_data set Item = "Smoothie" where Price_Per_Unit =4;
+
+update sales_data set Item = "na" where Price_Per_Unit = 0;
+update sales_data set Location = "Online" where Location = "na" or Location is null; /* set Online Where Location is null or na*/
+update sales_data set Payment_Method = "UPI" where Payment_Method = "na" or Payment_Method is null; /* set UPI where value is na or is null */
+
+SELECT DISTINCT Location FROM sales_data;
+
+SET SQL_SAFE_UPDATES = 1;
+
+select Transaction_date from sales_data;
+
+select * from sales_data;
+
+SELECT Transaction_Date FROM sales_data LIMIT 5;
